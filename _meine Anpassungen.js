2253b1f0ce452d1ed937faa36588d204efcc0fb2
Alex-at-gthb <as_200508@gmx.de>
2020-11-30 16:00:39 +0100

    Delete _meine Anpassungen.js

--- _meine Anpassungen.js
+++
@@ -1,7 +0,0 @@
-Hints.characters = 'asdfgqwertyxcvb'; // for german keyboard
-settings.hintAlign = "right";
-
-mapkey('yn', "#7Copy current page's title and URL", function() {
-    const str = document.title + '\n' + window.location.href + '\n';
-    Clipboard.write(str);
-});
